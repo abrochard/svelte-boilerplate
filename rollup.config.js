b50c523e9ef9e2ff05b21d6b2003e97886e7584f
Adrien Brochard <aaa.brochard@gmail.com>
2020-07-05 13:48:57 -0400

    updating dep

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,8 @@
 import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import builtins from 'rollup-plugin-node-builtins';
 
 export default {
   input: 'src/main.js',
@@ -13,6 +16,12 @@ export default {
         css.write('public/main.css');
       },
     }),
-    resolve()
+    resolve({
+      browser: true,
+	  dedupe: ['svelte']
+    }),
+    commonjs(),
+    json(),
+    builtins()
   ]
 };
